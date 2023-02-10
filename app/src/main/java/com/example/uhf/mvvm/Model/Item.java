6e8335c9496728910bb5204b59fd60f3a4033779
Janko <jankojovicic351@gmail.com>
2023-02-10 13:08:19 +0100

    Live data implementation MVVM architecture.

--- app/src/main/java/com/example/uhf/mvvm/Model/Item.java
+++ app/src/main/java/com/example/uhf/mvvm/Model/Item.java
@@ -1,5 +1,56 @@
 package com.example.uhf.mvvm.Model;
 
-@Entity()
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "item")
 public class Item {
+
+    @PrimaryKey(autoGenerate = false)
+    private String ecd;
+
+    private String ident;
+
+    private String name;
+
+    private int location;
+
+    public Item(String ecd, String ident, String name, int location) {
+        this.ecd = ecd;
+        this.ident = ident;
+        this.name = name;
+        this.location = location;
+    }
+
+    public void setEcd(String ecd) {
+        this.ecd = ecd;
+    }
+
+    public void setIdent(String ident) {
+        this.ident = ident;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLocation(int location) {
+        this.location = location;
+    }
+
+    public String getEcd() {
+        return ecd;
+    }
+
+    public String getIdent() {
+        return ident;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getLocation() {
+        return location;
+    }
 }
---
+++ app/src/main/java/com/example/uhf/mvvm/Model/ItemDAO.java
@@ -0,0 +1,23 @@
+package com.example.uhf.mvvm.Model;
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+@Dao
+public interface ItemDAO {
+    @Insert
+    void insert(Item item);
+    @Update
+    void update(Item item);
+    @Delete
+    void delete(Item item);
+    @Query("DELETE FROM item")
+    void deleteAllItems();
+    @Query("SELECT * FROM item")
+    LiveData<List<Item>> getAllItems();
+}
