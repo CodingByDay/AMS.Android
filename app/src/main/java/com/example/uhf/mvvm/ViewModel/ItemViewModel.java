0b9f2ae4c18ee0ba9ae0407e953eaa392fc58ea5
Janko <jankojovicic351@gmail.com>
2023-02-13 12:52:25 +0100

    Activities and fragments.

--- app/src/main/java/com/example/uhf/mvvm/ViewModel/ItemViewModel.java
+++ app/src/main/java/com/example/uhf/mvvm/ViewModel/ItemViewModel.java
@@ -1,13 +1,41 @@
 package com.example.uhf.mvvm.ViewModel;
 
+import android.app.Application;
 import android.util.Log;
 
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
 import androidx.lifecycle.ViewModel;
 
-public class ItemViewModel extends ViewModel {
+import com.example.uhf.ItemRepository;
+import com.example.uhf.mvvm.Model.Item;
+
+import java.util.List;
+
+public class ItemViewModel extends AndroidViewModel {
+    private ItemRepository repository;
+    private LiveData<List<Item>> allItems;
+
+    public ItemViewModel(@NonNull Application application) {
+        super(application);
+        repository = new ItemRepository(application);
+        allItems = repository.getAllItems();
+    }
+    public void insert(Item item) {
+        repository.insert(item);
+    }
 
 
-    public ItemViewModel() {
-        Log.i("ItemViewModel", "View model is created");
+    public void update (Item item) {
+        repository.update(item);
+    }
+
+    public void delete (Item item) {
+        repository.delete(item);
+    }
+
+    public LiveData<List<Item>> getAllItems() {
+        return allItems;
     }
 }
