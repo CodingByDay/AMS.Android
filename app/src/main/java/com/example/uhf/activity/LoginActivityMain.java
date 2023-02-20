a3ddaf1a9713526b99b3207e481e47fec732ff3e
Janko <jankojovicic351@gmail.com>
2023-02-20 13:29:39 +0100

    Custom action bar

--- app/src/main/java/com/example/uhf/activity/LoginActivityMain.java
+++ app/src/main/java/com/example/uhf/activity/LoginActivityMain.java
@@ -3,26 +3,38 @@ package com.example.uhf.activity;
 import android.app.Activity;
 
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+import android.widget.Toolbar;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.uhf.R;
 
-public class LoginActivityMain extends Activity {
+public class LoginActivityMain extends AppCompatActivity {
 private Button login;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_main);
+        getSupportActionBar().show();
 
         initPageViews();
 
 
 
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
     }
 
     private void initPageViews() {
