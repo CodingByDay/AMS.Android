4fbaac5dd8a33831f067588cb1084992bc0612ec
Janko <jankojovicic351@gmail.com>
2023-02-24 12:44:19 +0100

    Barcode reading implementation

--- app/src/main/java/com/example/uhf/activity/LocationActivity.java
+++ app/src/main/java/com/example/uhf/activity/LocationActivity.java
@@ -5,12 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import android.app.Activity;
+import android.app.Dialog;
 import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -24,6 +28,7 @@ import com.example.uhf.view.UhfLocationCanvasView;
 import com.rscja.deviceapi.RFIDWithUHFUART;
 import com.rscja.deviceapi.interfaces.IUHF;
 import com.rscja.deviceapi.interfaces.IUHFLocationCallback;
+import com.toptoche.searchablespinnerlibrary.SearchableSpinner;
 
 import java.util.List;
 import java.util.Objects;
@@ -118,7 +123,44 @@ public class LocationActivity extends AppCompatActivity {
         });
     }
 
+    public class LocationDialog {
+        public void showDialog(AppCompatActivity activity){
+            final Dialog dialog = new Dialog(activity);
+            dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+            dialog.setCancelable(false);
+            dialog.setContentView(R.layout.add_location_alert);
+            EditText tbLocationScan = (EditText) dialog.findViewById(R.id.tbLocationScan);
+            SearchableSpinner cbLocation = (SearchableSpinner) dialog.findViewById(R.id.cbLocation);
+            Button btYes = (Button) dialog.findViewById(R.id.btYes);
+            Button btNo = (Button) dialog.findViewById(R.id.btNo);
+            // dialog dismiss
+            btYes.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
 
+                }
+            });
+            btNo.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                }
+            });
+            cbLocation.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                @Override
+                public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+                }
+
+                @Override
+                public void onNothingSelected(AdapterView<?> adapterView) {
+
+                }
+            });
+
+            dialog.show();
+        }
+    }
     private void startLocation(){
         String epc=etEPC.getText().toString();
         boolean result= mReader.startLocation(this,epc, IUHF.Bank_EPC,32, new IUHFLocationCallback(){
@@ -134,13 +176,16 @@ public class LocationActivity extends AppCompatActivity {
                         @Override
                         public void onClick(DialogInterface dialog, int which) {
                             dialog.dismiss();
+                            stopLocation();
+                            LocationDialog alert = new LocationDialog();
+                            alert.showDialog(LocationActivity.this);
                         }
                     });
                     alert.setNegativeButton("Ne", new DialogInterface.OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialog, int which) {
                             dialog.dismiss();
-                            stopLocation();
+                            startLocation();
                         }
                     });
                     alert.show();
